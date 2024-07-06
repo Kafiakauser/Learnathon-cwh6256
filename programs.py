b6388627a78c652ddacb492760248dc5d41bdd1f
Kafiakauser <148422606+Kafiakauser@users.noreply.github.com>
2024-07-07 01:09:32 +0530

    Add files via upload

--- programs.py
+++ programs.py
@@ -26,3 +26,36 @@ print("\nResults: ")
 print("\nTotal cost before tax : ${:.2f}",format(total_cost_before_tax))
 print("\nTax amount (10%): ${:.2f}",format(tax_amount))
 print("\nTotal cost after tax: ${:.2f}",format(total_cost_after_tax))
+
+
+
+#programs on variables & Data Types
+length = float(input("Enter the length of the rectanle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+#calculation
+area = length * width 
+
+#Display the results 
+print("\nResults: ")
+print("Area of rectangle: {:.2f} sqquare units",format(area))
+
+
+#Input Varaiables
+a = int(input("Enter the value to the variable : "))
+b = int(input("Enter the value to the variable : "))
+
+#Displaying values before swapping 
+print("Before Swapping: ")
+print("a =",a)
+print("b =",b)
+
+# Swapping the values 
+a = a + b 
+b = a - b 
+a = a - b 
+
+# Displaying the values after swapping 
+print("\nAfater swapping: ")
+print("a =",a)
+print("b =",b)
