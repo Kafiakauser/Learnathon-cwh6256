47d71217423f7924a311ee19b897b706a4916450
Kafiakauser <148422606+Kafiakauser@users.noreply.github.com>
2024-07-07 02:32:36 +0530

    Add files via upload

---
+++ keywords,typecasting&strings.py
@@ -0,0 +1,50 @@
+#Program to concatenate 2 strings
+
+#Input
+string1 = input("Enter the first string : ")
+string2 = input("Enter the second string: ")
+
+# Concatenate strings
+result_string = string1 + string2
+
+#Output
+print("Concatenated String :", result_string)
+
+
+#Find the Length of a String
+
+#Input 
+input_string = input("Enter a string: ")
+
+#Find the length of a string
+string_length = len(input_string)
+
+#output
+print("Length of a string : ", string_length)
+
+
+
+#Reverse a String
+
+#input
+input_string = input("Enter a String: ")
+
+#Reverse the string 
+reversed_string = input_string[::-1]
+
+#output
+print("Reversed String : ", reversed_string)
+
+
+
+
+#Cover String to Uppercase
+
+#input
+input_string = input("Enter a string: ")
+
+#Convert to uppercase
+uppercase_string = input_string.upper()
+
+#output
+print("Uppercase String : ", uppercase_string)
